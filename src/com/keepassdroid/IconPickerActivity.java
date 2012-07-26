628ee04100ad2273f34eae7c4f1a5fd9c2391199
Andrei Popleteev <contact@popleteev.com>
2012-07-26 17:28:26 -0430

    Adapted icon picker UI

--- src/com/keepassdroid/IconPickerActivity.java
+++ src/com/keepassdroid/IconPickerActivity.java
@@ -25,10 +25,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -36,83 +38,81 @@ import android.widget.TextView;
 import com.android.keepass.R;
 import com.keepassdroid.icons.Icons;
 
-public class IconPickerActivity extends LockCloseActivity
-{
+public class IconPickerActivity extends LockCloseActivity {
 	public static final String KEY_ICON_ID = "icon_id";
 
-	public static void Launch(Activity act)
-	{
+	public static void Launch(Activity act) {
 		Intent i = new Intent(act, IconPickerActivity.class);
 		act.startActivityForResult(i, 0);
 	}
 
-   @Override
-   public void onCreate(Bundle savedInstanceState)
-   {
-   	super.onCreate(savedInstanceState);
-   	setContentView(R.layout.icon_picker);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.icon_picker);
 
-   	GridView currIconGridView = (GridView)findViewById(R.id.IconGridView);
-   	currIconGridView.setAdapter(new ImageAdapter(this));
+		GridView currIconGridView = (GridView) findViewById(R.id.IconGridView);
+		currIconGridView.setAdapter(new ImageAdapter(this));
 
-   	currIconGridView.setOnItemClickListener(new OnItemClickListener()
-   	{
-			public void onItemClick(AdapterView<?> parent, View v, int position, long id)
-			{
+		currIconGridView.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View v,
+					int position, long id) {
 				final Intent intent = new Intent();
-				
+
 				intent.putExtra(KEY_ICON_ID, position);
 				setResult(EntryEditActivity.RESULT_OK_ICON_PICKER, intent);
-				
+
 				finish();
 			}
-   	});
-   }
-   
-   public class ImageAdapter extends BaseAdapter
-   {
-   	Context mContext;
+		});
+		setupBackButton();
+	}
 
-   	public ImageAdapter(Context c)
-   	{
-   		mContext = c;
-   	}
-   	
-   	public int getCount()
-   	{
-   		/* Return number of KeePass icons */
-   		return Icons.count();
-   	}
-   	
-   	public Object getItem(int position)
-		{
+	private void setupBackButton() {
+		Button backButton = (Button) findViewById(R.id.back_button);
+		backButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				IconPickerActivity.this.finish();
+			}
+		});
+	}
+
+	public class ImageAdapter extends BaseAdapter {
+		Context mContext;
+
+		public ImageAdapter(Context c) {
+			mContext = c;
+		}
+
+		public int getCount() {
+			/* Return number of KeePass icons */
+			return Icons.count();
+		}
+
+		public Object getItem(int position) {
 			return null;
 		}
 
-		public long getItemId(int position)
-		{
+		public long getItemId(int position) {
 			return 0;
 		}
-   	
-   	public View getView(int position, View convertView, ViewGroup parent)
-   	{
-   		View currView;
-   		if(convertView == null)
-   		{
-   			LayoutInflater li = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE); 
-   			currView = li.inflate(R.layout.icon, null);
-   		}
-   		else
-   		{
-   			currView = convertView;
-   		}
-   		
-   		TextView tv = (TextView) currView.findViewById(R.id.icon_text);
-   		tv.setText("" + position);
-   		ImageView iv = (ImageView) currView.findViewById(R.id.icon_image);
-   		iv.setImageResource(Icons.iconToResId(position));
- 
-   		return currView;
-   	}
-   }
+
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View currView;
+			if (convertView == null) {
+				LayoutInflater li = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				currView = li.inflate(R.layout.icon, null);
+			} else {
+				currView = convertView;
+			}
+
+			TextView tv = (TextView) currView.findViewById(R.id.icon_text);
+			tv.setText("" + position);
+			ImageView iv = (ImageView) currView.findViewById(R.id.icon_image);
+			iv.setImageResource(Icons.iconToResId(position));
+
+			return currView;
+		}
+	}
 }
