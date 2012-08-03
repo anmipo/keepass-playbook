4cd65859a9e5becc0067f5e4186de81daf520e8c
Andrei Popleteev <contact@popleteev.com>
2012-08-03 07:01:23 -0430

    Added basic icons (parent, folder, generic file, kdb file)

--- src/com/anmipo/playbook/filelist/FileListActivity.java
+++ src/com/anmipo/playbook/filelist/FileListActivity.java
@@ -1,23 +1,24 @@
 package com.anmipo.playbook.filelist;
 
 import java.io.File;
-import java.io.FileDescriptor;
-import java.io.FilenameFilter;
 import java.util.Arrays;
 import java.util.Comparator;
 
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.EditText;
-import android.widget.ListAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.keepass.R;
 import com.keepassdroid.intents.Intents;
@@ -92,6 +93,7 @@ public class FileListActivity extends ListActivity implements OnItemClickListene
 		File dir = new File(currentDirectory);
 		if (dir.isDirectory()) {
 			listAdapter.setItems(dir.listFiles());
+			setSelection(0);
 		} else {
 			throw new RuntimeException("strange file object");
 		}
@@ -102,11 +104,40 @@ public class FileListActivity extends ListActivity implements OnItemClickListene
 		editText.setText(currentFileName);		
 	}
 
-	private class FileAdapter extends ArrayAdapter<String> {
+	
+	@Override
+	public void onItemClick(AdapterView<?> listView, View view, int position, long id) {
+		if (position == 0) {
+			// "go to parent" clicked
+			int pos = currentDirectory.lastIndexOf(File.separatorChar, 
+					currentDirectory.length() - 1);
+			if (pos > 0) {
+				showDirectory(currentDirectory.substring(0, pos));
+			} else {
+				showDirectory(ROOT);
+			}
+		} else {
+			// some file or folder clicked
+			FileAdapter adapter = (FileAdapter) listView.getAdapter(); 
+			File item = adapter.getFileItem(position);
+			if (item.isDirectory()) {
+				showDirectory(currentDirectory + item.getName() + File.separatorChar);
+			} else {
+				setFileName(item.getName());
+			}
+		}
+	}
+	
+	private class FileAdapter extends BaseAdapter {
 		private File[] items;
+		private Drawable upIcon, folderIcon, fileIcon, kdbIcon; 
+		
 		public FileAdapter(Context context) {
-			super(context, android.R.layout.simple_list_item_1);
 			items = null;
+			upIcon = getResources().getDrawable(R.drawable.ic_file_parent); 
+			folderIcon = getResources().getDrawable(R.drawable.ic_file_folder); 
+			fileIcon = getResources().getDrawable(R.drawable.ic_file_generic); 
+			kdbIcon = getResources().getDrawable(R.drawable.ic_file_kdb);
 		}
 		public void setItems(File[] items) {
 			this.items = items;
@@ -140,31 +171,49 @@ public class FileListActivity extends ListActivity implements OnItemClickListene
 				return item.getName();
 			}
 		}
-		// TODO remove this when switch to BaseAdapter is complete
-		public File getFileItem(int position) {
+		private File getFileItem(int position) {
 			return items[position - 1];
 		}
-	}
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			TextView textView;
+			if (convertView == null) {
+				textView = (TextView) getLayoutInflater().inflate(
+						android.R.layout.simple_list_item_1, null);
+				textView.setCompoundDrawablePadding(10);
+			} else {
+				textView = (TextView) convertView;
+			}
+			textView.setText(getItem(position));
+			
+			textView.setCompoundDrawablesWithIntrinsicBounds(
+					getItemIcon(position), null, null, null);
+			
+			return textView;
+		}
 
-	@Override
-	public void onItemClick(AdapterView<?> listView, View view, int position, long id) {
-		if (position == 0) {
-			// "go to parent" clicked
-			int pos = currentDirectory.lastIndexOf(File.separatorChar, 
-					currentDirectory.length() - 1);
-			if (pos > 0) {
-				showDirectory(currentDirectory.substring(0, pos));
+		private Drawable getItemIcon(int position) {
+			Drawable result;
+			if (position == 0) {
+				result = upIcon;
 			} else {
-				showDirectory(ROOT);
+				File item = getFileItem(position);
+				if (item.isDirectory()) {
+					result = folderIcon;
+				} else {
+					String fileName = item.getName();
+					if (fileName. endsWith(".kdb") || fileName.endsWith(".kdbx")) {
+						result = kdbIcon;
+					} else {
+						result = fileIcon;
+					}
+				}
 			}
-		} else {
-			FileAdapter adapter = (FileAdapter) listView.getAdapter(); 
-			File item = adapter.getFileItem(position);
-			if (item.isDirectory()) {
-				showDirectory(currentDirectory + item.getName() + File.separatorChar);
-			} else {
-				setFileName(item.getName());
-			}
+			return result;
 		}
 	}
 }
