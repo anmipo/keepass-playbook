427b16f1650ef886729051b780a0ce4846693cd6
Andrei Popleteev <contact@popleteev.com>
2012-08-13 18:52:00 -0430

    Added GPL header to new files

--- src/com/anmipo/playbook/filelist/FileUtils.java
+++ src/com/anmipo/playbook/filelist/FileUtils.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2012 Andrei Popleteev.
+ *     
+ * This file is part of KeePass for BlackBerry PlayBook.
+ *
+ *  KeePass for BlackBerry PlayBook is free software: you can redistribute it 
+ *  and/or modify it under the terms of the GNU General Public License 
+ *  as published by the Free Software Foundation, either version 2 
+ *  of the License, or (at your option) any later version.
+ *
+ *  KeePass for BlackBerry PlayBook is distributed in the hope that 
+ *  it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with KeePass for BlackBerry PlayBook.  
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.anmipo.playbook.filelist;
 
 import java.io.File;
